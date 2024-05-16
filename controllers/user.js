d8116733b7c178719dc9efbcce915219cbf007f8
Maks <maxkov19861411@gmail.com>
2024-05-16 11:39:38 +0300

    Added and updated new files

---
+++ controllers/user.js
@@ -0,0 +1,29 @@
+import User from "../models/user.js";
+import path from "path";
+import fs from "node:fs/promises";
+
+export const updateAvatar = async (req, res, next) => {
+  const { _id } = req.user;
+  try {
+    await fs.rename(
+      req.file.path,
+      path.resolve("public/avatars", req.file.filname)
+    );
+    const user = await User.findByIdAndUpdate(
+      _id,
+      { avatar: req.file.filname },
+      { new: true }
+    );
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+  // const { _id } = req.user;
+  // const { path: tmpUpload, originalName } = req.file;
+  // const resultUpload = path.join(avatarsDir, originalName);
+
+  // await fs.rename(tmpUpload, resultUpload);
+
+  // const avatarURL = path.join("avatars", originalName);
+  // await User.findByIdAndUpdate(_id, { avatarURL });
+};
